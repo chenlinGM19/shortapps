ed57dcd1d20771b6aae278600f94aa340db9c729
chenlinGM19 <60935905+chenlinGM19@users.noreply.github.com>
2026-02-05 00:20:03 +0800

    Update EditorActivity.java

--- app/src/main/java/com/shortapps/app/EditorActivity.java
+++ app/src/main/java/com/shortapps/app/EditorActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
